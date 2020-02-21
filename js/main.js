9b798d37fbf7d6e28d6eaa2ed747abaa51cf2e5c
1 <kingifean@gmail.com>
2020-02-21 01:35:49 +0100

    initail commit..

---
+++ js/main.js
@@ -0,0 +1,47 @@
+  
+  	var loaderPage = function() {
+      $(".fh5co-loader").fadeOut("slow");
+    };
+
+    $(function(){
+      loaderPage();
+    });
+  
+  $('.autoplay').slick({
+    dots: true,
+    infinite: false,
+    speed: 2000,
+    autoplay: true,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: false
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          dots: false
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          dots: false
+        }
+      }
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+    ]
+  });
